3f92545beb9d4e854351e59e03c6adf8ae7d1350
Santiago03092024 <santiagocarmendez@gmail.com>
2026-02-18 14:03:56 -0400

    database.py

--- database.py
+++ database.py
@@ -1,18 +1,17 @@
+# -*- coding: utf-8 -*-
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Intenta obtener la URL de la base de datos de la nube (Render)
-# Si no existe, usa la local (fruver.db)
+# Logica inteligente: Si hay nube usa nube, si no, usa archivo local
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./fruver.db")
 
-# Corrección para que funcione en Render (ellos usan postgres:// y Python quiere postgresql://)
-if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+# Parche para Render (ellos usan postgres:// y python quiere postgresql://)
+if SQLALCHEMY_DATABASE_URL and SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
     SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
-# Configuración híbrida
-if "sqlite" in SQLALCHEMY_DATABASE_URL:
+if SQLALCHEMY_DATABASE_URL and "sqlite" in SQLALCHEMY_DATABASE_URL:
     engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 else:
     engine = create_engine(SQLALCHEMY_DATABASE_URL)
